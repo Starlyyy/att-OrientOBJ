41831fc0fe0df90db2a3fa54c918f8ce8f266285
Izabella da Costa <130591081+Starlyyy@users.noreply.github.com>
2024-10-03 15:49:36 -0300

    Create Pedido.php

---
+++ aula8/modelo/Pedido.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once("Prato.php");
+
+class Pedido {
+    private string $nomeCliente;
+    private string $nomeGarcom;
+    private Prato $prato;
+
+    public function __toString()
+    {
+        return sprintf("Cliente: %s | Garçom: %s | Prato:  %d | %s | %.2f |", $this->getNomeCliente(), $this->getNomeGarcom(), $this->getPrato()->getNumero(), $this->getPrato()->getNome(), $this->getPrato()->getValor());
+    }
+
+    public function getNomeCliente(): string
+    {
+        return $this->nomeCliente;
+    }
+
+    public function setNomeCliente(string $nomeCliente): self
+    {
+        $this->nomeCliente = $nomeCliente;
+
+        return $this;
+    }
+
+    public function getNomeGarcom(): string
+    {
+        return $this->nomeGarcom;
+    }
+
+    public function setNomeGarcom(string $nomeGarcom): self
+    {
+        $this->nomeGarcom = $nomeGarcom;
+
+        return $this;
+    }
+
+    public function getPrato(): Prato
+    {
+        return $this->prato;
+    }
+
+    public function setPrato(Prato $prato): self
+    {
+        $this->prato = $prato;
+
+        return $this;
+    }
+}
