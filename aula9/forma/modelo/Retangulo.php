99d3ec643992e58a71bb9f9db9e0aeee0561a23a
Izabella da Costa <130591081+Starlyyy@users.noreply.github.com>
2024-10-17 15:31:15 -0300

    Create Retangulo.php

---
+++ aula9/forma/modelo/Retangulo.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('IFormaGeometrica.php');
+
+class Retangulo implements IFormaGeometrica{
+    private $base; 
+    private $altura;
+
+    public function getArea(){
+        return $this->base * $this->altura;
+    }
+
+    public function getDesenho(){
+        return "┌───────────────────────────────┐\n".
+               "│                               │\n" .
+               "│                               │\n" .
+               "│                               │\n" .
+               "│                               │\n" .
+               "│                               │\n" .
+               "│                               │\n" .
+               "│                               │\n" .
+               "└───────────────────────────────┘\n";
+    }
+
+    public function getBase()
+    {
+        return $this->base;
+    }
+
+    public function setBase($base): self
+    {
+        $this->base = $base;
+
+        return $this;
+    }
+
+    public function getAltura()
+    {
+        return $this->altura;
+    }
+
+    public function setAltura($altura): self
+    {
+        $this->altura = $altura;
+
+        return $this;
+    }
+}
